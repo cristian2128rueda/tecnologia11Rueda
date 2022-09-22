401be209afc41d150898cecfa644c192bd5303b2
cristian2128rueda <114179208+cristian2128rueda@users.noreply.github.com>
2022-09-22 12:29:30 -0500

    Add files via upload

---
+++ ejercicio 5-rueda.py
@@ -0,0 +1,30 @@
+print("Bienvenido al programa, calculadora de ganancias para su empresa.")
+print("")
+nombre1=input("Ingrese su primer Nombre : ",)
+nombre2=input("Ingrese su Segundo Nombre : ",)
+apellido1=input("Ingrese su primer Apellido : ",)
+apellido2=input("Ingrese su segundo Apellido : ",)
+print("")
+empresa=input("Nombre de la Empresa: ")
+print("")
+print("")
+print("Bienvenido al sistema ", nombre1, nombre2, apellido1, apellido2 )
+print("")
+print("")
+print(" Empresa:" + empresa )
+print("")
+print("")
+
+GananciasEnero=int(input("Ingrese las ganancias de Enero: "))
+GananciasFebrero=int(input("Ingrese las ganancias de Febrero: "))
+GananciasMarzo=int(input("Ingrese las ganancias de Marzo: "))
+print("")
+print("")
+
+
+gananciastotales= GananciasEnero +GananciasFebrero +GananciasMarzo
+print("Las ganancias fueron de $: ",gananciastotales)
+print("")
+print("")
+
+print("Gracias por utilizar nuestros servicios, los esperamos pronto")
